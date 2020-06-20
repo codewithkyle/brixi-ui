5c64a22a17c48c1696d7418bcc91deaceacb0f55
Kyle Andrews <codingwithkyle@gmail.com>
2020-06-20 09:03:16 -0400

    Adds deployment and GH Actions

---
+++ build-tools/deploy.js
@@ -0,0 +1,16 @@
+const ghPages = require('gh-pages');
+require('dotenv').config();
+
+ghPages.publish('build', {
+    user: {
+        name: process.env.NAME,
+        email: process.env.EMAIL
+    },
+    repo: 'https://' + process.env.ACCESS_TOKEN +'@github.com/' + process.env.USERNAME + '/' + process.env.PROJECT + '.git',
+    silent: true
+}, (error)=>{
+    if (error)
+    {
+        console.log(error);
+    }
+});
