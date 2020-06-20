a4ef1a87fe1334a4a57bc5904bde167912628946
Kyle Andrews <codingwithkyle@gmail.com>
2020-06-20 09:30:45 -0400

    Updates URL

--- build-tools/deploy.js
+++ build-tools/deploy.js
@@ -1,16 +1,19 @@
-const ghPages = require('gh-pages');
-require('dotenv').config();
+const ghPages = require("gh-pages");
+require("dotenv").config();
 
-ghPages.publish('public', {
-    user: {
-        name: process.env.NAME,
-        email: process.env.EMAIL
-    },
-    repo: 'https://' + process.env.ACCESS_TOKEN +'@github.com/' + process.env.USERNAME + '/' + process.env.PROJECT + '.git',
-    silent: true
-}, (error)=>{
-    if (error)
+ghPages.publish(
+    "public",
     {
-        console.log(error);
+        user: {
+            name: process.env.NAME,
+            email: process.env.EMAIL,
+        },
+        repo: "https://" + process.env.ACCESS_TOKEN + "@github.com/" + process.env.USERNAME + "/" + process.env.PROJECT + ".git",
+        silent: true,
+    },
+    (error) => {
+        if (error) {
+            console.log(error);
+        }
     }
-});
+);
--- build-tools/renderer.js
+++ build-tools/renderer.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 const path = require("path");
 const cwd = process.cwd();
 
-const publicDir = path.join(cwd, 'public');
-if (fs.existsSync(publicDir)){
-    fs.rmdirSync(publicDir, {recursive: true});
+const publicDir = path.join(cwd, "public");
+if (fs.existsSync(publicDir)) {
+    fs.rmdirSync(publicDir, { recursive: true });
 }
 fs.mkdirSync(publicDir);
 
-const shellFile = path.join(cwd, 'lib', 'shell.html');
-fs.copyFileSync(shellFile, path.join(publicDir, 'index.html'));
+const shellFile = path.join(cwd, "lib", "shell.html");
+fs.copyFileSync(shellFile, path.join(publicDir, "index.html"));
 
-const cnameFile = path.join(cwd, 'lib', 'CNAME');
-fs.copyFileSync(cnameFile, path.join(publicDir, 'CNAME'));
+const cnameFile = path.join(cwd, "lib", "CNAME");
+fs.copyFileSync(cnameFile, path.join(publicDir, "CNAME"));
