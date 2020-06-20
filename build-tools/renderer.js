9ca50fe806f3d71acc5e433f663ff874111ebc7a
Kyle Andrews <codingwithkyle@gmail.com>
2020-06-20 09:07:58 -0400

    Adds CNAME file to deployment

--- build-tools/renderer.js
+++ build-tools/renderer.js
@@ -10,3 +10,6 @@ fs.mkdirSync(publicDir);
 
 const shellFile = path.join(cwd, 'lib', 'shell.html');
 fs.copyFileSync(shellFile, path.join(publicDir, 'index.html'));
+
+const cnameFile = path.join(cwd, 'lib', 'CNAME');
+fs.copyFileSync(cnameFile, path.join(publicDir, 'CNAME'));
