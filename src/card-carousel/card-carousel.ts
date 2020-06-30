0307f9a833ff2fab6e3b4aaef3f2d7c36aa8dd89
Kyle Andrews <codingwithkyle@gmail.com>
2020-06-30 08:48:56 -0400

    Adds card carousel

---
+++ src/card-carousel/card-carousel.ts
@@ -0,0 +1,36 @@
+class CardCarousel extends HTMLElement {
+    private nextButton: HTMLElement;
+    private backButton: HTMLElement;
+    private container: HTMLElement;
+    private cardWidth: number;
+
+    constructor() {
+        super();
+        this.nextButton = this.querySelector(".js-next");
+        this.backButton = this.querySelector(".js-back");
+        this.container = this.querySelector(".js-container");
+        this.cardWidth = 320;
+    }
+
+    private handleNextClick: EventListener = () => {
+        this.container.scrollBy({
+            top: 0,
+            left: this.cardWidth,
+            behavior: "smooth",
+        });
+    };
+
+    private handleBackClick: EventListener = () => {
+        this.container.scrollBy({
+            top: 0,
+            left: -this.cardWidth,
+            behavior: "smooth",
+        });
+    };
+
+    connectedCallback() {
+        this.nextButton.addEventListener("click", this.handleNextClick);
+        this.backButton.addEventListener("click", this.handleBackClick);
+    }
+}
+customElements.define("card-carousel", CardCarousel);
