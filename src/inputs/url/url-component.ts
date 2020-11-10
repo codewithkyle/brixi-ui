bb3e022224cb8f39801c334adeeca170239eeddc
Kyle Andrews <codingwithkyle@gmail.com>
2020-11-10 14:32:56 -0500

    Closes #6 - added URL input

---
+++ src/inputs/url/url-component.ts
@@ -0,0 +1,65 @@
+export default class URLComponent extends HTMLElement {
+    private input: HTMLInputElement;
+    private textEl: HTMLElement;
+    private errorEl: HTMLElement;
+
+    constructor() {
+        super();
+        this.input = this.querySelector("input");
+        this.textEl = this.querySelector("p");
+        const errorEl = document.createElement("p");
+        errorEl.className = "error";
+        errorEl.style.display = "none";
+        this.insertBefore(errorEl, this.input);
+        this.errorEl = errorEl;
+    }
+
+    private validateInput() {
+        if (this.input.required) {
+            if (this.input.value === "") {
+                if (this.getAttribute("state") !== "invalid") {
+                    this.reportError("This field is required.");
+                }
+            } else {
+                if (new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/g).test(this.input.value)){
+                    this.clearError();
+                }else{
+                    if (this.getAttribute("state") !== "invalid") {
+                        this.reportError("Invalid URL format.");
+                    }
+                }
+                
+            }
+        } else {
+            this.clearError();
+        }
+    }
+
+    public reportError(error: string) {
+        this.errorEl.innerHTML = error;
+        this.errorEl.style.display = "block";
+        if (this.textEl){
+            this.textEl.style.display = "none";
+        }
+        this.setAttribute("state", "invalid");
+    }
+
+    public clearError() {
+        this.errorEl.style.display = "none";
+        if (this.textEl){
+            this.textEl.style.display = "block";
+        }
+        this.setAttribute("state", "valid");
+    }
+
+    private handleBlur: EventListener = () => {
+        this.validateInput();
+    };
+
+    private handleInput: EventListener = this.clearError.bind(this);
+
+    connectedCallback() {
+        this.input.addEventListener("input", this.handleInput);
+        this.input.addEventListener("blur", this.handleBlur);
+    }
+}
