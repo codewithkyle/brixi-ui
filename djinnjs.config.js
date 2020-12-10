5671fef0f02ed2fec2ecead25668b5477a2a832a
Kyle Andrews <codingwithkyle@gmail.com>
2020-12-10 15:12:33 -0500

    Closed #14 - reworked data table component

--- djinnjs.config.js
+++ djinnjs.config.js
@@ -1,3 +1,4 @@
 module.exports = {
     src: ["./_compiled", "./_css"],
+    minimumConnection: "slow-2g"
 };
--- src/data-table/data-table.ts
+++ src/data-table/data-table.ts
@@ -1,5 +1,6 @@
 // @ts-expect-error
 import { html, render } from "https://unpkg.com/lit-html?module";
+import { Component } from "djinnjs/component";
 
 type RowData = {
     col1: string;
@@ -9,104 +10,30 @@ type RowData = {
     col5: string;
 };
 
-export default class DataTable extends HTMLElement {
+type DataTableState = {
+    rows: Array<RowData>;
+    page: number;
+    totalPages: number;
+    rowsPerPage: number;
+};
+
+export default class DataTable extends Component<DataTableState> {
     private foot: HTMLElement;
     private body: HTMLElement;
-    private nextButton: HTMLButtonElement;
-    private backButton: HTMLButtonElement;
     private emptyMessage: HTMLElement;
-    private pageCount: HTMLElement;
-
-    private page: number;
-    private totalPages: number;
-    private data: Array<RowData>;
-
-    private rowsPerPage: number;
 
     constructor() {
         super();
         this.foot = this.querySelector("tfoot");
         this.body = this.querySelector("tbody");
-        this.nextButton = this.querySelector(".js-next");
-        this.backButton = this.querySelector(".js-back");
-        this.pageCount = this.querySelector(".js-page-count");
         this.emptyMessage = this.querySelector("empty-message");
 
-        this.page = 0;
-        this.totalPages = 0;
-        this.data = [];
-
-        // Config
-        this.rowsPerPage = 3;
-    }
-
-    private render(rows: Array<RowData>) {
-        // Reset views
-        this.body.innerHTML = "";
-        if (rows.length === 0) {
-            this.body.style.display = "none";
-            this.foot.style.display = "none";
-            this.emptyMessage.style.display = "block";
-        } else {
-            this.body.style.display = "block";
-            this.foot.style.display = "block";
-            this.emptyMessage.style.display = "none";
-        }
-
-        // Render tbody data
-        for (let i = 0; i < rows.length; i++) {
-            const row = document.createElement("tr");
-
-            const cell1 = document.createElement("td");
-            cell1.innerText = rows[i].col1;
-            row.appendChild(cell1);
-
-            const cell2 = document.createElement("td");
-            cell2.innerText = rows[i].col2;
-            row.appendChild(cell2);
-
-            const cell3 = document.createElement("td");
-            cell3.innerText = rows[i].col3;
-            row.appendChild(cell3);
-
-            const cell4 = document.createElement("td");
-            cell4.innerText = rows[i].col4;
-            row.appendChild(cell4);
-
-            const cell5 = document.createElement("td");
-            cell5.innerText = rows[i].col5;
-            row.appendChild(cell5);
-
-            this.body.appendChild(row);
-        }
-
-        // Updates tfoot data
-        if (this.totalPages > 1) {
-            this.foot.style.display = "block";
-            this.pageCount.innerText = `Page ${this.page + 1} of ${this.totalPages}`;
-            if (this.page === 0) {
-                this.backButton.disabled = true;
-            } else {
-                this.backButton.disabled = false;
-            }
-            if (this.page === this.totalPages - 1) {
-                this.nextButton.disabled = true;
-            } else {
-                this.nextButton.disabled = false;
-            }
-        } else {
-            this.foot.style.display = "block";
-        }
-    }
-
-    private filter() {
-        const filteredData = [...this.data];
-
-        // filter data
-
-        const rows = filteredData.slice(this.page * this.rowsPerPage, this.page * this.rowsPerPage + this.rowsPerPage);
-        this.totalPages = Math.ceil(filteredData.length / this.rowsPerPage);
-        this.render(rows);
+        this.state = {
+            rowsPerPage: 3,
+            page: 0,
+            totalPages: 0,
+            rows: [],
+        };
     }
 
     private async fetchData() {
@@ -120,8 +47,10 @@ export default class DataTable extends HTMLElement {
         if (reqeust.ok) {
             const response = await reqeust.json();
             if (response.success) {
-                this.data = response.data;
-                this.filter();
+                this.setState({
+                    rows: response.data,
+                    totalPages: Math.ceil(response.data.length / this.state.rowsPerPage),
+                });
             } else {
                 const error = response?.message || response?.error || "Something went wrong loading table data.";
                 console.error(error);
@@ -131,25 +60,82 @@ export default class DataTable extends HTMLElement {
         }
     }
 
-    private handleNext: EventListener = () => {
-        this.page++;
-        if (this.page > this.totalPages - 1) {
-            this.page = this.totalPages - 1;
+    public next() {
+        const updatedState = { ...this.state };
+        updatedState.page++;
+        if (updatedState.page > this.state.totalPages - 1) {
+            updatedState.page = this.state.totalPages - 1;
         }
-        this.filter();
-    };
+        this.setState(updatedState);
+    }
 
-    private handleBack: EventListener = () => {
-        this.page--;
-        if (this.page < 0) {
-            this.page = 0;
+    public back() {
+        const updatedState = { ...this.state };
+        updatedState.page--;
+        if (updatedState.page < 0) {
+            updatedState.page = 0;
         }
-        this.filter();
-    };
+        this.setState(updatedState);
+    }
 
-    connectedCallback() {
-        this.nextButton.addEventListener("click", this.handleNext);
-        this.backButton.addEventListener("click", this.handleBack);
+    connected() {
         this.fetchData();
+    }
+
+    render() {
+        if (!this.state.rows.length) {
+            this.foot.style.display = "none";
+            this.body.style.display = "none";
+            this.emptyMessage.innerText = "Failed to load. Try refreshing the page.";
+            this.emptyMessage.style.display = "block";
+        } else {
+            this.body.style.display = "block";
+            this.emptyMessage.style.display = "none";
+
+            const rows: Array<RowData> = this.state.rows.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage);
+            const body = html`
+                ${rows.map((row) => {
+                    return html`
+                        <tr>
+                            ${Object.values(row).map((value) => {
+                                return html`<td>${value}</td>`;
+                            })}
+                        </tr>
+                    `;
+                })}
+            `;
+            render(body, this.body);
+
+            if (this.state.totalPages > 1) {
+                this.foot.style.display = "block";
+
+                const foot = html`
+                    <tr>
+                        <td>Page ${this.state.page + 1} of ${this.state.totalPages}</td>
+                        <td>
+                            <button @click=${(e) => this.back()} ?disabled=${this.state.page === 0}>
+                                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                    <path
+                                        fill="currentColor"
+                                        d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"
+                                    ></path>
+                                </svg>
+                            </button>
+                        </td>
+                        <td>
+                            <button @click=${(e) => this.next()} ?disabled=${this.state.page === this.state.totalPages - 1}>
+                                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                    <path
+                                        fill="currentColor"
+                                        d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+                                    ></path>
+                                </svg>
+                            </button>
+                        </td>
+                    </tr>
+                `;
+                render(foot, this.foot);
+            }
+        }
     }
 }
