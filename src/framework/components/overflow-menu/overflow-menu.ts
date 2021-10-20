f119a3a6c944e4c755576278dbb9ae276f5ab13c
Kyle Andrews <codingwithkyle@gmail.com>
2021-10-20 12:38:49 -0400

    Overflow menu components now cacl their fixed pposition

--- src/framework/components/overflow-menu/overflow-menu.ts
+++ src/framework/components/overflow-menu/overflow-menu.ts
@@ -10,9 +10,8 @@ export interface OverflowItem {
     danger?: boolean,
 };
 export interface IOverflowMenu {
-    icon: "vertical" | "horizontal",
+    icon: string,
     items: Array<OverflowItem>,
-    position: "left" | "right",
     tooltip: string,
     class: string,
     css: string,
@@ -22,8 +21,7 @@ export interface IOverflowMenu {
 };
 export interface OverflowMenuSettings{
     items: Array<OverflowItem>,
-    icon?: "vertical" | "horizontal" | string,
-    position?: "left" | "right",
+    icon?: string,
     tooltip?: string,
     css?: string,
     class?: string,
@@ -35,9 +33,8 @@ export default class OverflowMenu extends SuperComponent<IOverflowMenu>{
     constructor(settings:OverflowMenuSettings){
         super();
         this.model = {
-            icon: "vertical",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" /></svg>`,
             items: [],
-            position: "right",
             tooltip: null,
             class: "",
             css: "",
@@ -53,22 +50,32 @@ export default class OverflowMenu extends SuperComponent<IOverflowMenu>{
         });
     }
 
-    private renderIcon(){
-        switch (this.model.icon){
-            case "horizontal":
-                return html`
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
-                    </svg>
-                `;
-            case "vertical":
-                return html`
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-                    </svg>
-                `;
-            default:
-                return html`${unsafeHTML(this.model.icon)}`;
+    override connected(){
+        document.addEventListener("click", () => {
+            const container = this.querySelector("overflow-container") as HTMLElement;
+            if (container){
+                container.classList.remove("is-visible");
+            }    
+        }, { passive: true, capture: true});
+    }
+
+    private handleClick:EventListener = (e:Event) => {
+        const target = e.currentTarget as HTMLElement;
+        const bounds = target.getBoundingClientRect();
+        const container = this.querySelector("overflow-container") as HTMLElement;
+        if (container){
+            const containerBounds = container.getBoundingClientRect();
+            let top = bounds.top + bounds.height;
+            if (top + containerBounds.height >= window.innerHeight){
+                top = bounds.top - containerBounds.height;
+            }
+            let left = bounds.left;
+            if (left + containerBounds.width >= window.innerWidth){
+                left = bounds.left + bounds.width - containerBounds.width;
+            }
+            container.style.top = `${top}px`;
+            container.style.left = `${left}px`;
+            container.classList.toggle("is-visible");
         }
     }
 
@@ -95,10 +102,10 @@ export default class OverflowMenu extends SuperComponent<IOverflowMenu>{
             this.setAttribute(key, `${this.model.attributes[key]}`);
         });
         const view = html`
-            <button sfx="button" type="button" aria-label="${this.model.tooltip || "open overflow menu"}" ?tooltip=${this.model.tooltip}>
-                ${this.renderIcon()}
+            <button @click=${this.handleClick} sfx="button" type="button" aria-label="${this.model.tooltip || "open menu"}" tooltip>
+                ${unsafeHTML(this.model.icon)}
             </button>
-            <overflow-container class="${this.model.position}">
+            <overflow-container>
                 ${this.model.items.map(item => {
                     return this.renderItem(item);
                 })}
