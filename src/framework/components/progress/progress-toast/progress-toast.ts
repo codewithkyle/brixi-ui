74ffaf75a2fa0e63c0e66eeb7d09dfa2f5d42c72
Kyle Andrews <codingwithkyle@gmail.com>
2021-12-13 07:31:11 -0500

    Fixing progress toast font

--- src/framework/components/progress/progress-toast/progress-toast.ts
+++ src/framework/components/progress/progress-toast/progress-toast.ts
@@ -78,8 +78,8 @@ export default class ProgressToast extends SuperComponent<IProgressToast> {
                 color: "white",
             })}
             <div class="ml-0.75" flex="column wrap" style="flex:1;">
-                <h2 class="block font-bold font-md">${this.model.title}</h2>
-                <h3 class="${this.model.subtitle?.length ? "block" : "none"} font-sm font-grey-700">${this.model.subtitle}</h3>
+                <h2 class="block font-medium font-base mb-0.25 font-white">${this.model.title}</h2>
+                <h3 class="${this.model.subtitle?.length ? "block" : "none"} font-sm font-grey-200">${this.model.subtitle}</h3>
             </div>
         `;
         setTimeout(() => {
