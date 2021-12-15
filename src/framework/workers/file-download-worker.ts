c277c34dffe34860a67e2d6b2bacbd006837f06f
Kyle Andrews <codingwithkyle@gmail.com>
2021-12-15 09:52:24 -0500

    Switched to larger file

--- src/framework/workers/file-download-worker.ts
+++ src/framework/workers/file-download-worker.ts
@@ -52,6 +52,9 @@ async function fetchData(url: RequestInfo, options: RequestInit) {
                 const { done, value } = await reader.read();
                 buffer.set(value, recieved);
                 tick(value.byteLength);
+                await new Promise((resolve) => {
+                    setTimeout(resolve, 4000);
+                });
                 if (done) {
                     break;
                 }
