bfea6b97c5fc540639cbbbfc38ca729980732706
Kyle Andrews <codingwithkyle@gmail.com>
2021-12-24 09:33:21 -0500

    🐛 Fixed prerelease script errors

--- build/types.js
+++ build/types.js
@@ -6,29 +6,15 @@ const glob = require("glob");
 const tempDir = path.join(cwd, "_types");
 const files = glob.sync(`${tempDir}/**/*.d.ts`);
 
-const nodeModulesDir = path.join(cwd, "node_modules");
-const moduleFiles = glob.sync(`${nodeModulesDir}/**/*.d.ts`);
-
-for (let i = 0; i < files.length; i++){
-    const filename = files[i].replace(/.*[\/\\]/, "").trim();
-    for (let k = 0; k < moduleFiles.length; k++){
-        const moduleFilename = moduleFiles[k].replace(/.*[\/\\]/, "").trim();
-        if (filename === moduleFilename){
-            fs.copyFileSync(moduleFiles[k], files[i]);
-            break;
-        }
-    }
-}
-
 const outDir = path.join(cwd, "dist", "framework", "types");
-if (fs.existsSync(outDir)){
-    fs.rmdirSync(outDir, {recursive: true});
+if (fs.existsSync(outDir)) {
+    fs.rmdirSync(outDir, { recursive: true });
 }
-fs.mkdirSync(outDir);
+fs.mkdirSync(outDir, { recursive: true });
 
-for (let i = 0; i < files.length; i++){
+for (let i = 0; i < files.length; i++) {
     const filename = files[i].replace(/.*[\/\\]/, "");
     fs.renameSync(files[i], path.join(outDir, filename));
 }
 
-fs.rmdirSync(tempDir, {recursive: true});
+fs.rmdirSync(tempDir, { recursive: true });
