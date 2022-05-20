902b414d646451fccd16daddec51ef1575d37b9e
Kyle Andrews <codingwithkyle@gmail.com>
2022-05-20 07:22:00 -0400

    ✨ Added prod file download button to navigation

--- src/sandbox/nav.ts
+++ src/sandbox/nav.ts
@@ -194,6 +194,35 @@ export default class Nav extends SuperComponent<NavData> {
                                 ></path>
                             </svg>
                         </a>
+                        <a
+                            sfx="button"
+                            target="_blank"
+                            rel="noopener"
+                            tooltip="Download"
+                            href="/static/brixi-ui.zip"
+                            class="bttn"
+                            shape="round"
+                            kind="text"
+                            color="grey"
+                            icon="center"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                stroke-width="2"
+                                stroke="currentColor"
+                                fill="none"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            >
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M19 18a3.5 3.5 0 0 0 0 -7h-1a5 4.5 0 0 0 -11 -2a4.6 4.4 0 0 0 -2.1 8.4"></path>
+                                <line x1="12" y1="13" x2="12" y2="22"></line>
+                                <polyline points="9 19 12 22 15 19"></polyline>
+                            </svg>
+                        </a>
                     </div>
                     ${this.model.navigation.map((link) => {
                         return html` ${link.children.length ? this.renderLinkWithChildren(link) : this.renderLink(link)} `;
