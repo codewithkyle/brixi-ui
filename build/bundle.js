f26b40cd5ee703ab9d93639140088f108a8356d2
Kyle Andrews <codingwithkyle@gmail.com>
2022-10-01 11:25:03 -0400

    ♻ Tabs can be sortable & fixed bundle renaming DX

--- build/bundle.js
+++ build/bundle.js
@@ -6,7 +6,17 @@ const install = require("esinstall").install;
 
 (async () => {
 	const pkg = require(path.join(cwd, "package.json"));
-	await install(pkg.bundle);
+    const bundle = [];
+    const remaps = {};
+    for (let i = 0; i < pkg.bundle.length; i++){
+        if (typeof pkg.bundle[i] === "string"){
+            bundle.push(pkg.bundle[i]);
+        } else {
+            bundle.push(pkg.bundle[i].in);
+            remaps[pkg.bundle[i].in.replace(/.*[\/\\]/, "")] = pkg.bundle[i].out;
+        }
+    }
+	await install(bundle);
 	
 	const libDir = path.join(cwd, "src", "framework", "lib");
 	if (fs.existsSync(libDir)) {
@@ -17,7 +27,10 @@ const install = require("esinstall").install;
 	const scripts = glob.sync(`${path.join(cwd, "web_modules")}/**/*.js`);
 	for (let i = 0; i < scripts.length; i++){
 		const script = scripts[i];
-		const filename = script.replace(/.*[\/\\]/, "");
+		let filename = script.replace(/.*[\/\\]/, "");
+        if (filename in remaps){
+            filename = remaps[filename];
+        }
 		fs.renameSync(script, path.join(libDir, filename));
 	}
 
--- src/framework/components/tabs/tabs.ts
+++ src/framework/components/tabs/tabs.ts
@@ -3,30 +3,33 @@ import SuperComponent from "@codewithkyle/supercomponent";
 import env from "~brixi/controllers/env";
 import { noop, parseDataset } from "~brixi/utils/general";
 import { unsafeHTML } from "lit-html/directives/unsafe-html";
+import Sortable from "sortablejs";
 
-export interface Tab {
+export interface ITab {
     label: string;
     value: string | number;
-    icon?: string;
+    icon?: string | HTMLElement;
 }
 export interface ITabs {
-    tabs: Array<Tab>;
-    callback: (tab: string) => void;
+    tabs: Array<ITab>;
+    callback: (tab: string | number) => void;
     active: number;
     css: string;
     class: string;
     attributes: {
         [name: string]: string | number;
     };
+    sortable: boolean;
 }
 export interface TabsSettings {
-    tabs: Array<Tab>;
-    callback: (tab: string) => void;
+    tabs: Array<ITab>;
+    callback: (tab: string | number) => void;
     css?: string;
     class?: string;
     attributes?: {
         [name: string]: string | number;
     };
+    sortable?: boolean;
 }
 export default class Tabs extends SuperComponent<ITabs> {
     constructor(settings: TabsSettings) {
@@ -38,6 +41,7 @@ export default class Tabs extends SuperComponent<ITabs> {
             css: "",
             class: "",
             attributes: {},
+            sortable: false,
         };
         this.model = parseDataset<ITabs>(this.dataset, this.model);
         env.css(["tabs"]).then(() => {
@@ -46,41 +50,108 @@ export default class Tabs extends SuperComponent<ITabs> {
         });
     }
 
-    private renderIcon(tab) {
-        let out;
-        if (tab?.icon?.length) {
-            out = html` <i> ${unsafeHTML(tab.icon)} </i> `;
-        } else {
-            out = "";
-        }
-        return out;
+    /*
+     * Returns the rendered order of the tabs.
+     * Use the array of tab values to determine the order.
+     */
+    public getOrder(): Array<string | number> {
+        const values = [];
+        this.querySelectorAll("tab-component").forEach((tab: Tab) => {
+            values.push(tab.model.value);
+        });
+        return values;
     }
 
-    private handleClick: EventListener = (e: Event) => {
-        const target = e.currentTarget as HTMLElement;
-        this.model.callback(target.dataset.value);
-        this.update({
-            active: parseInt(target.dataset.index),
+    public callback(value: string | number, index: number) {
+        this.set(
+            {
+                active: index,
+            },
+            true
+        );
+        this.model.callback(value);
+        this.querySelectorAll("tab-component").forEach((tab: Tab) => {
+            if (tab.index === index) {
+                tab.classList.add("is-active");
+            } else {
+                tab.classList.remove("is-active");
+            }
         });
-    };
+    }
+
+    private sort() {
+        const tabsContainer = this.querySelector("tabs-container");
+        Sortable.create(tabsContainer);
+    }
 
     override render() {
+        console.log(this.model);
         this.className = this.model.class;
         this.style.cssText = this.model.css;
         Object.keys(this.model.attributes).map((key) => {
             this.setAttribute(key, `${this.model.attributes[key]}`);
         });
         const view = html`
-            ${this.model.tabs.map((tab, index) => {
-                const isActive = index === this.model.active;
-                return html`
-                    <button class="${isActive ? "is-active" : ""}" data-value="${tab.value}" data-index="${index}" @click=${this.handleClick} sfx="button">
-                        ${this.renderIcon(tab)} ${tab.label}
-                    </button>
-                `;
-            })}
+            <tabs-container>
+                ${this.model.tabs.map((tab, index) => {
+                    const isActive = index === this.model.active;
+                    return new Tab(tab, this, isActive, index);
+                })}
+            </tabs-container>
         `;
         render(view, this);
+        if (this.model.sortable) {
+            setTimeout(this.sort.bind(this), 80);
+        }
     }
 }
-env.mount("tabs-component", Tabs);
+class Tab extends SuperComponent<ITab> {
+    private isActive: boolean;
+    private parent: Tabs;
+    public index: number;
+
+    constructor(tab: ITab, parent: Tabs, active: boolean, index: number) {
+        super();
+        this.model = tab;
+        this.isActive = active;
+        this.parent = parent;
+        this.index = index;
+        this.render();
+    }
+
+    private handleClick = (e) => {
+        this.parent.callback(this.model.value, this.index);
+    };
+
+    private renderIcon() {
+        let out;
+        if (this.model?.icon instanceof HTMLElement) {
+            out = html`<i>${this.model.icon}</i>`;
+        } else if (typeof this.model?.icon === "string" && this.model?.icon?.length) {
+            out = html` <i> ${unsafeHTML(this.model.icon)} </i> `;
+        } else {
+            out = "";
+        }
+        return out;
+    }
+
+    override render() {
+        const view = html` ${this.renderIcon()} ${this.model.label} `;
+        this.tabIndex = 0;
+        this.setAttribute("sfx", "button");
+        this.className = this.isActive ? "is-active" : "";
+        this.setAttribute("role", "button");
+        this.setAttribute("aria-label", `Open ${this.model.label}`);
+        render(view, this);
+    }
+
+    override connected() {
+        this.addEventListener("click", this.handleClick);
+    }
+
+    override disconnected(): void {
+        this.removeEventListener("click", this.handleClick);
+    }
+}
+env.bind("tab-component", Tab);
+env.bind("tabs-component", Tabs);
