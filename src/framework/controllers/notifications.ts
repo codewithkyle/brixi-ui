f5e57ef484281e4eaf9988aad2ff21d0577b9ca7
Kyle Andrews <codingwithkyle@gmail.com>
2022-11-20 13:13:27 -0500

    ✨ Added ability to limit notification duration (still defaults to Infinity)

--- src/framework/controllers/notifications.ts
+++ src/framework/controllers/notifications.ts
@@ -20,13 +20,14 @@ class Notifications {
         actions: Array<{
             label: string;
             callback: Function;
-        }> = []
+        }> = [],
+        duration: number = Infinity
     ) {
         toast({
             title: title,
             message: message,
             icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`,
-            duration: Infinity,
+            duration: duration,
             closeable: true,
             buttons: actions,
         });
@@ -42,14 +43,15 @@ class Notifications {
         actions: Array<{
             label: string;
             callback: Function;
-        }> = []
+        }> = [],
+        duration: number = Infinity
     ) {
         toast({
             title: title,
             message: message,
             icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`,
             classes: ["-green"],
-            duration: Infinity,
+            duration: duration,
             closeable: true,
             buttons: actions,
         });
@@ -65,14 +67,15 @@ class Notifications {
         actions: Array<{
             label: string;
             callback: Function;
-        }> = []
+        }> = [],
+        duration: number = Infinity
     ) {
         toast({
             title: title,
             message: message,
             icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>`,
             classes: ["-yellow"],
-            duration: Infinity,
+            duration: duration,
             closeable: true,
             buttons: actions,
         });
@@ -88,14 +91,15 @@ class Notifications {
         actions: Array<{
             label: string;
             callback: Function;
-        }> = []
+        }> = [],
+        duration: number = Infinity
     ) {
         toast({
             title: title,
             message: message,
             icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`,
             classes: ["-red"],
-            duration: Infinity,
+            duration: duration,
             closeable: true,
             buttons: actions,
         });
