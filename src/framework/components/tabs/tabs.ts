82aac8e1819b25b5498c88aa7397c6f45d121c40
Kyle Andrews <codingwithkyle@gmail.com>
2023-02-23 17:37:37 -0500

    🎨 Adjusted 'add tab' button vertical position

--- src/framework/components/tabs/tabs.ts
+++ src/framework/components/tabs/tabs.ts
@@ -142,6 +142,7 @@ export default class Tabs extends SuperComponent<ITabs> {
                 icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>`,
                 iconPosition: "center",
                 shape: "round",
+                css: "margin-bottom:6px;",
                 callback: this.addTab.bind(this),
             });
         } else {
