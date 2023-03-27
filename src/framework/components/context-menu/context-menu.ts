ae602a810d5625ac650aa3ecae91ac139361034c
Kyle Andrews <codingwithkyle@gmail.com>
2023-03-27 17:03:49 -0400

    ✨ Added Context Menu component

---
+++ src/framework/components/context-menu/context-menu.ts
@@ -0,0 +1,85 @@
+import { html, render, TemplateResult } from "lit-html";
+import SuperComponent from "@codewithkyle/supercomponent";
+import env from "~brixi/controllers/env";
+import pos from "~brixi/controllers/pos";
+
+export interface ContextMenuItem {
+    label: string;
+    hotkey?: string;
+    callback: Function;
+}
+export interface IContextMenu {
+    items: ContextMenuItem[];
+    x: number;
+    y: number;
+}
+export interface ContextMenuSettings {
+    items: ContextMenuItem[];
+    x: number;
+    y: number;
+}
+export default class ContextMenu extends SuperComponent<IContextMenu> {
+    constructor(settings: ContextMenuSettings) {
+        super();
+        this.model = {
+            items: [],
+            x: 0,
+            y: 0,
+        };
+        env.css(["context-menu"]).then(() => {
+            this.set(settings);
+        });
+    }
+
+    override connected() {
+        document.addEventListener(
+            "click",
+            () => {
+                this.remove();
+            },
+            { passive: true, capture: true }
+        );
+        window.addEventListener(
+            "resize",
+            () => {
+                this.remove();
+            },
+            { passive: true, capture: true }
+        );
+        window.addEventListener(
+            "scroll",
+            () => {
+                this.remove();
+            },
+            { passive: true, capture: true }
+        );
+        this.addEventListener("click", (e: Event) => {
+            e.stopImmediatePropagation();
+        });
+    }
+
+    private handleItemClick: EventListener = (e: Event) => {
+        const target = e.currentTarget as HTMLElement;
+        const index = parseInt(target.dataset.index);
+        this.model.items?.[index]?.callback();
+    };
+
+    private renderItem(item: ContextMenuItem, index: number): TemplateResult {
+        if (item === null) {
+            return html`<hr />`;
+        }
+        return html`
+            <button sfx="button" type="button" @click=${this.handleItemClick} data-index="${index}">
+                <span>${item.label}</span>
+                ${item.hotkey ? html`<span class="font-grey-400">${item.hotkey}</span>` : ""}
+            </button>
+        `;
+    }
+
+    override render() {
+        pos.positionElement(this, this.model.x, this.model.y);
+        const view = html` ${this.model.items?.map((item, index) => this.renderItem(item, index))} `;
+        render(view, this);
+    }
+}
+env.bind("context-menu", ContextMenu);
