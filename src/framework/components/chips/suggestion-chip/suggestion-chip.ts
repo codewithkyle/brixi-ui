f06fab9289fb21cc336966d13fe1b616744eb0f3
Kyle Andrews <codingwithkyle@gmail.com>
2023-07-11 12:16:58 -0400

    ♻ Refactored Suggestion Chip component

--- src/framework/components/chips/suggestion-chip/suggestion-chip.ts
+++ src/framework/components/chips/suggestion-chip/suggestion-chip.ts
@@ -1,54 +1,41 @@
 import { html, render } from "lit-html";
-import SuperComponent from "@codewithkyle/supercomponent";
 import env from "~brixi/controllers/env";
-import { noop, parseDataset } from "~brixi/utils/general";
+import { parseDataset } from "~brixi/utils/general";
+import Component from "~brixi/component";
+
+env.css(["suggestion-chip"]);
 
 export interface ISuggestionChip {
-    css: string;
-    class: string;
-    attributes: {
-        [name: string]: string | number;
-    };
     label: string;
     value: string | number;
-    callback: Function;
 }
-export interface SuggestionChipSettings {
-    css?: string;
-    class?: string;
-    attributes?: {
-        [name: string]: string | number;
-    };
-    label: string;
-    value: string | number;
-    callback: Function;
-}
-export default class SuggestionChip extends SuperComponent<ISuggestionChip> {
-    constructor(settings: SuggestionChipSettings) {
+export default class SuggestionChip extends Component<ISuggestionChip> {
+    constructor() {
         super();
         this.model = {
-            css: "",
-            class: "",
-            attributes: {},
-            label: null,
+            label: "",
             value: null,
-            callback: noop,
         };
-        this.model = parseDataset<ISuggestionChip>(this.dataset, this.model);
-        env.css(["suggestion-chip"]).then(() => {
-            this.set(settings, true);
-            this.render();
-        });
+    }
+
+    static get observedAttributes() {
+        return ["data-label", "data-value"];
     }
 
     override connected(): void {
+        const settings = parseDataset(this.dataset, this.model);
+        this.set(settings);
         this.addEventListener("click", this.handleClick);
         this.addEventListener("keyup", this.handleKeyup);
         this.addEventListener("keydown", this.handleKeydown);
     }
 
     private handleClick = () => {
-        this.model.callback(this.model.value);
+        this.dispatchEvent(new CustomEvent("suggest", {
+            detail: {
+                value: this.model.value,
+            }
+        }));
     };
 
     private handleKeydown = (e: KeyboardEvent) => {
@@ -60,18 +47,14 @@ export default class SuggestionChip extends SuperComponent<ISuggestionChip> {
     private handleKeyup = (e: KeyboardEvent) => {
         if (e.key === " ") {
             this.classList.remove("is-active");
-            this.model.callback(this.model.value);
+            this.click();
         }
     };
 
     override render() {
-        this.className = this.model.class;
-        this.style.cssText = this.model.css;
-        Object.keys(this.model.attributes).map((key) => {
-            this.setAttribute(key, `${this.model.attributes[key]}`);
-        });
         this.tabIndex = 0;
         this.setAttribute("role", "button");
+        this.setAttribute("sfx", "button");
         const view = html` <span>${this.model.label}</span> `;
         render(view, this);
     }
