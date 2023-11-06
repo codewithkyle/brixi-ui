20ede8afe063c241c8508144b16040b783d656c5
Kyle Andrews <codingwithkyle@gmail.com>
2023-11-05 19:49:00 -0500

    ✨ Added dark mode overflow

--- brixi.config.js
+++ brixi.config.js
@@ -100,7 +100,7 @@ module.exports = {
     },
     shadows: {
         colors: {
-            grey: "var(--black-hsl)",
+            black: "var(--black-hsl)",
         },
         sizes: {
             sm: `
