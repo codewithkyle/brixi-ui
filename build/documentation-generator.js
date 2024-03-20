c9659d5605a95fdf4209831f84b5074fa81bf6d4
Kyle Andrews <codingwithkyle@gmail.com>
2024-03-20 15:57:10 -0400

    🐛 Fixed MPA component docs

--- build/documentation-generator.js
+++ build/documentation-generator.js
@@ -3,24 +3,40 @@ const path = require("path");
 const cwd = process.cwd();
 const glob = require("glob");
 
-const publicDir = path.join(cwd, "public", "docs", "components", "spa");
-const componentsDir = path.join(cwd, "src", "spa", "components");
+const spaPublicDir = path.join(cwd, "public", "docs", "components", "spa");
+const mpaPublicDir = path.join(cwd, "public", "docs", "components", "mpa");
+const spaComponentsDir = path.join(cwd, "src", "spa", "components");
+const mpaComponentsDir = path.join(cwd, "src", "mpa");
 
 // TODO: mpa documentation
 
 (async () => {
-    if (fs.existsSync(publicDir)) {
-        await fs.promises.rm(publicDir, { recursive: true });
+    if (fs.existsSync(spaPublicDir)) {
+        await fs.promises.rm(spaPublicDir, { recursive: true });
     }
-    await fs.promises.mkdir(publicDir, { recursive: true });
-    const docs = glob.sync(`${componentsDir}/**/*.md`);
-    for (const doc of docs) {
+    if (fs.existsSync(mpaPublicDir)) {
+        await fs.promises.rm(mpaPublicDir, { recursive: true });
+    }
+    await fs.promises.mkdir(spaPublicDir, { recursive: true });
+    await fs.promises.mkdir(mpaPublicDir, { recursive: true });
+    const spaDocs = glob.sync(`${spaComponentsDir}/**/*.md`);
+    for (const doc of spaDocs) {
         const name = doc
             .match(/.*[\\\/]/, "")[0]
             .replace(/[\\\/]$/, "")
             .replace(/.*[\\\/]/, "")
             .toLowerCase()
             .trim();
-        await fs.promises.copyFile(doc, path.join(publicDir, `${name}.md`));
+        await fs.promises.copyFile(doc, path.join(spaPublicDir, `${name}.md`));
+    }
+    const mpaDocs = glob.sync(`${mpaComponentsDir}/**/*.md`);
+    for (const doc of mpaDocs) {
+        const name = doc
+            .match(/.*[\\\/]/, "")[0]
+            .replace(/[\\\/]$/, "")
+            .replace(/.*[\\\/]/, "")
+            .toLowerCase()
+            .trim();
+        await fs.promises.copyFile(doc, path.join(mpaPublicDir, `${name}.md`));
     }
 })();
