bd709afcf366edc905a8ba020e05ecf46e971c0d
Kyle Andrews <codingwithkyle@gmail.com>
2024-03-20 15:31:10 -0400

    ✨ Added MPA component source code

--- build/raw-generator.js
+++ build/raw-generator.js
@@ -3,18 +3,25 @@ const path = require("path");
 const cwd = process.cwd();
 const glob = require("glob");
 
-const publicDir = path.join(cwd, "public", "raw", "components", "spa");
-const componentsDir = path.join(cwd, "src", "spa", "components");
+const publicSPADir = path.join(cwd, "public", "raw", "components", "spa");
+const publicMPADir = path.join(cwd, "public", "raw", "components", "mpa");
+const spaComponentsDir = path.join(cwd, "src", "spa", "components");
+const mpaComponentsDir = path.join(cwd, "src", "mpa", "components");
 const cssDir = path.join(cwd, "public", "css");
-const jsDir = path.join(cwd, "public", "spa", "js");
+const spajsDir = path.join(cwd, "public", "spa", "js");
+const mpajsDir = path.join(cwd, "public", "mpa", "js");
 
 (async () => {
-    if (fs.existsSync(publicDir)) {
-        await fs.promises.rm(publicDir, { recursive: true });
+    if (fs.existsSync(publicSPADir)) {
+        await fs.promises.rm(publicSPADir, { recursive: true });
     }
-    await fs.promises.mkdir(publicDir, { recursive: true });
-    const components = glob.sync(`${componentsDir}/**/index.html`);
-    for (const file of components) {
+    if (fs.existsSync(publicMPADir)) {
+        await fs.promises.rm(publicMPADir, { recursive: true });
+    }
+    await fs.promises.mkdir(publicSPADir, { recursive: true });
+    await fs.promises.mkdir(publicMPADir, { recursive: true });
+    const spaComponents = glob.sync(`${spaComponentsDir}/**/index.html`);
+    for (const file of spaComponents) {
         const name = file
             .match(/.*[\\\/]/, "")[0]
             .replace(/[\\\/]$/, "")
@@ -28,9 +35,9 @@ const jsDir = path.join(cwd, "public", "spa", "js");
         const readme = path.join(lDir, "readme.md");
         const ts = path.join(lDir, `${name}.ts`);
         const css = path.join(cssDir, `${name}.css`);
-        const js = path.join(jsDir, `${name}.js`);
+        const js = path.join(spajsDir, `${name}.js`);
         const html = file;
-        const pDir = path.join(publicDir, name);
+        const pDir = path.join(publicSPADir, name);
         if (!fs.existsSync(pDir)) {
             await fs.promises.mkdir(pDir);
         }
@@ -39,6 +46,39 @@ const jsDir = path.join(cwd, "public", "spa", "js");
         }
         if (fs.existsSync(ts)) {
             await fs.promises.copyFile(ts, path.join(pDir, `${name}.ts`));
+        }
+        if (fs.existsSync(css)) {
+            await fs.promises.copyFile(css, path.join(pDir, `${name}.css`));
+        }
+        if (fs.existsSync(js)) {
+            await fs.promises.copyFile(js, path.join(pDir, `${name}.js`));
+        }
+        if (fs.existsSync(html)) {
+            await fs.promises.copyFile(html, path.join(pDir, `${name}.html`));
+        }
+    }
+    const mpaComponents = glob.sync(`${mpaComponentsDir}/**/index.html`);
+    for (const file of mpaComponents) {
+        const name = file
+            .match(/.*[\\\/]/, "")[0]
+            .replace(/[\\\/]$/, "")
+            .replace(/.*[\\\/]/, "")
+            .toLowerCase()
+            .trim();
+        const lDir = file
+            .match(/.*[\\\/]/, "")[0]
+            .toLowerCase()
+            .trim();
+        const readme = path.join(lDir, "readme.md");
+        const css = path.join(cssDir, `${name}.css`);
+        const js = path.join(mpajsDir, `${name}.js`);
+        const html = file;
+        const pDir = path.join(publicMPADir, name);
+        if (!fs.existsSync(pDir)) {
+            await fs.promises.mkdir(pDir);
+        }
+        if (fs.existsSync(readme)) {
+            await fs.promises.copyFile(readme, path.join(pDir, "readme.md"));
         }
         if (fs.existsSync(css)) {
             await fs.promises.copyFile(css, path.join(pDir, `${name}.css`));
